ae883867f085fb6650de2a67234fa00e46fe84b7
Izhaki <roey@gefri.org>
2020-08-14 22:58:47 +0100

    [site] Edit home page copy
    And make images monochrome for now.

--- site/src/Home.tsx
+++ site/src/Home.tsx
@@ -53,6 +53,7 @@ const useStyles = makeStyles({
     height: 'auto',
     display: 'block', // To get rid of bottom spacing
     margin: '0px auto',
+    filter: 'saturate(0)',
   },
   dropShadow: {
     boxShadow: `rgba(0, 0, 0, 0.55) 0px 9px 12px`,
@@ -162,7 +163,7 @@ export default function Home() {
         <Section
           title="Test Everywhere"
           body="Bundle tests with your app and load them dynamically on demand. Then run them everywhere."
-          sub="Even your mom's iPhone."
+          sub="Even on your mom's iPhone."
           img="images/test-everywhere.png"
           alt="A mobile phone, tablet and laptop with and app and tests labels on screen"
         />
@@ -178,7 +179,7 @@ export default function Home() {
               You can use both in your suite.
             </>
           }
-          sub="Vest can be extended to support Jest, Ava, and the likes."
+          sub="Vest can be extended to support the syntax of Jest, Ava, and the likes."
           img="images/poly-syntax.svg"
           alt="A word cloud with test related keywords"
           rtl
@@ -203,7 +204,7 @@ export default function Home() {
         <Section
           title="Zero Setup"
           body="Webpack is already set to compile your app. With Vest, you simply add your tests to that process. No extra setup. No extra compilation."
-          sub="Ever tried to test a Typescript React Component that imports a css module? Save the time and complexity involved in installing additional packages and setting-up custom transformers."
+          sub="Ever tried to test a Typescript React component that imports a css module? Save the time and complexity involved in installing additional packages and setting-up custom transformers."
           img="images/zero-setup.png"
           alt="A code snippet"
           dropShadow
@@ -218,8 +219,8 @@ export default function Home() {
         />
         <Section
           title="CI"
-          body="Tests run on CI by spinning a headless browser, or using Browsertsack el al."
-          sub="Surely you do that already."
+          body="Tests run on CI by spinning a headless browser."
+          sub="Or use BrowserStack el al."
           img="images/cl.svg"
           alt="Command line logo"
           rtl
@@ -227,7 +228,7 @@ export default function Home() {
         <Section
           title="Minimalisticomposition"
           body="Vest is composed of a few small packages. It adheres to the Minimal API Surface principle."
-          sub="No assertion, stubs, mocks or related API - use existing libraries of your choice for these."
+          sub="No assertion, stubs, mocks or related API - use existing libraries of choice for these."
           img="images/architecture.svg"
           alt="A diagram showing the component of Vest"
         />
